1cf5d2077d8636594040e7de5570602ab7d1ff97
Debashis Nath <debashisnath1992@gmail.com>
2018-08-19 21:16:30 +0530

    about us page pushh

---
+++ about_us.php
@@ -0,0 +1,157 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <title>Eazyprint | About Us</title>
+
+    <link rel="shortcut icon" href="img/core-img/favicon.ico">
+
+    <script src="js/jquery-2.2.4.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/settings.css">
+    <link rel="stylesheet" type="text/css" href="css/layers.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+
+
+    <link rel="stylesheet" href="css/core-style.css">
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <link href="css/opensans.css" rel="stylesheet">
+
+
+    <script type="text/javascript" src="js/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="js/revolution.extension.navigation.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+	<style>
+		body{
+			background:#f5f5f5;
+		}
+		.pad0{
+			padding:0px;
+			margin:0px;
+		}
+		.shp_cart{
+			height:40px;background:rgb(42, 43, 46);width:40px;border-radius:50%;
+			margin-left:2px;text-align:center;
+		}
+		.shp_cart:hover{
+			cursor:pointer;
+			background:#dd0017;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="container-fluid bg-dark">
+	as
+	</div>
+
+    <?php 
+		include("header.php");
+	?>
+	<!-- slider -->
+	
+	
+	<!-- 2nd row of products -->
+
+	<section style="margin-top:50px;margin-bottom:50px;">
+		<div class="container-fluid" style="">
+			<h1 class="center" style="text-align:center;">About Us</h1>
+			<p>&nbsp;</p>
+			<div class="row">
+				<div class="col-md-1"></div>
+				<div class="col-md-10">
+						<p style="text-align:center;">Eazyprint offers the best platform in India to customize Mobile Cases for 400+ phone & tablet models, Mugs, Plates, Key rings, Photo Frames, Fridge Magnets, Posters and more. We use the best transfer printing techniques to deliver the most unique products to you. Our design platform enables you to use your Images or Text to customize and design your products.</p>
+					
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-1"></div>
+				<div class="col-md-3">
+					<div class="border" style="text-align:center;">
+						<img src="images/hd-512.png" class="img-responsive" style="height:100px;border-radius:50%;">
+						<div class="product-details">
+							<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">HIGH QUALITY PRINTING</a></h5>
+								<p class="price" style="text-align:center;font-size:18px;margin-top:15px;">
+									<span class="price-new">We use the Best Printing Techniques available in the Industry today to ensure that the Print is Durable and it looks Great on your Product.</span>
+								</p>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="border" style="text-align:center;">
+						<img src="images/guarantee-checkmark-512.png" class="img-responsive" style="height:100px;border-radius:50%;">
+						<div class="product-details">
+							<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">SATISFACTION GUARANTEED</a></h5>
+								<p class="price" style="text-align:center;font-size:18px;margin-top:15px;">
+									<span class="price-new">All products are Quality Checked before getting shipped.<br/>If there's still any issue with the product, we will make it right.</span>
+								</p>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="border" style="text-align:center;">
+						<img src="images/free-shipping.png" class="img-responsive" style="height:100px;">
+						<div class="product-details">
+							<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+								<p class="price" style="text-align:center;font-size:18px;margin-top:15px;">
+									<span class="price-new">All our orders are dispatched within 2-4 working days of getting placed. Fast & Free shipping is applicable on all products sold on Eazyprint.</span>
+								</p>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-1"></div>
+				
+			</div>
+		</div>
+	</section>
+
+	<!-- 3rd row of products -->
+
+	
+    
+	<footer style="margin-top:50px;">
+		<div class="container-fluid">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">s</div>
+					<div class="col-md-3">s</div>
+					<div class="col-md-3">s</div>
+					<div class="col-md-3">s</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+    <!-- ***** All jQuery Plugins ***** -->
+
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="js/include-all-plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+
+
+
+</body>
+</html>
