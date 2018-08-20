b5e577c531eac8bb5fb0ea492959883c0e8f49ed
Debashis Nath <debashisnath1992@gmail.com>
2018-08-20 14:51:23 +0530

    faq push

--- about_us.php
+++ about_us.php
@@ -48,6 +48,189 @@
 			cursor:pointer;
 			background:#dd0017;
 		}
+		.timeline {
+    list-style: none;
+    padding: 20px 0 20px;
+    position: relative;
+}
+
+    .timeline:before {
+        top: 0;
+        bottom: 0;
+        position: absolute;
+        content: " ";
+        width: 3px;
+        background-color: #eeeeee;
+        left: 50%;
+        margin-left: -1.5px;
+    }
+
+    .timeline > li {
+        margin-bottom: 20px;
+        position: relative;
+    }
+
+        .timeline > li:before,
+        .timeline > li:after {
+            content: " ";
+            display: table;
+        }
+
+        .timeline > li:after {
+            clear: both;
+        }
+
+        .timeline > li:before,
+        .timeline > li:after {
+            content: " ";
+            display: table;
+        }
+
+        .timeline > li:after {
+            clear: both;
+        }
+
+        .timeline > li > .timeline-panel {
+            width: 46%;
+            float: left;
+            border: 1px solid #d4d4d4;
+            border-radius: 2px;
+            padding: 20px;
+            position: relative;
+            -webkit-box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+            box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+        }
+
+            .timeline > li > .timeline-panel:before {
+                position: absolute;
+                top: 26px;
+                right: -15px;
+                display: inline-block;
+                border-top: 15px solid transparent;
+                border-left: 15px solid #ccc;
+                border-right: 0 solid #ccc;
+                border-bottom: 15px solid transparent;
+                content: " ";
+            }
+
+            .timeline > li > .timeline-panel:after {
+                position: absolute;
+                top: 27px;
+                right: -14px;
+                display: inline-block;
+                border-top: 14px solid transparent;
+                border-left: 14px solid #f5f5f5;
+                border-right: 0 solid #f5f5f5;
+                border-bottom: 14px solid transparent;
+                content: " ";
+            }
+
+        .timeline > li > .timeline-badge {
+            color: #fff;
+            width: 50px;
+            height: 50px;
+            line-height: 50px;
+            font-size: 1.4em;
+            text-align: center;
+            position: absolute;
+            top: 16px;
+            left: 50%;
+            margin-left: -25px;
+            background-color: #999999;
+            z-index: 100;
+            border-top-right-radius: 50%;
+            border-top-left-radius: 50%;
+            border-bottom-right-radius: 50%;
+            border-bottom-left-radius: 50%;
+        }
+
+        .timeline > li.timeline-inverted > .timeline-panel {
+            float: right;
+        }
+
+            .timeline > li.timeline-inverted > .timeline-panel:before {
+                border-left-width: 0;
+                border-right-width: 15px;
+                left: -15px;
+                right: auto;
+            }
+
+            .timeline > li.timeline-inverted > .timeline-panel:after {
+                border-left-width: 0;
+                border-right-width: 14px;
+                left: -14px;
+                right: auto;
+            }
+
+			.timeline-badge.primary {
+				background-color: #2e6da4 !important;
+			}
+
+			.timeline-badge.success {
+				background-color: #3f903f !important;
+			}
+
+			.timeline-badge.warning {
+				background-color: #f0ad4e !important;
+			}
+
+			.timeline-badge.danger {
+				background-color: #d9534f !important;
+			}
+
+			.timeline-badge.info {
+				background-color: #5bc0de !important;
+			}
+
+			.timeline-title {
+				margin-top: 0;
+				color: inherit;
+			}
+
+			.timeline-body > p,
+			.timeline-body > ul {
+				margin-bottom: 0;
+			}
+
+				.timeline-body > p + p {
+					margin-top: 5px;
+				}
+
+			@media (max-width: 767px) {
+				ul.timeline:before {
+					left: 40px;
+				}
+
+				ul.timeline > li > .timeline-panel {
+					width: calc(100% - 90px);
+					width: -moz-calc(100% - 90px);
+					width: -webkit-calc(100% - 90px);
+				}
+
+				ul.timeline > li > .timeline-badge {
+					left: 15px;
+					margin-left: 0;
+					top: 16px;
+				}
+
+				ul.timeline > li > .timeline-panel {
+					float: right;
+				}
+
+					ul.timeline > li > .timeline-panel:before {
+						border-left-width: 0;
+						border-right-width: 15px;
+						left: -15px;
+						right: auto;
+					}
+
+					ul.timeline > li > .timeline-panel:after {
+						border-left-width: 0;
+						border-right-width: 14px;
+						left: -14px;
+						right: auto;
+					}
+			}
 	</style>
 </head>
 
@@ -62,7 +245,7 @@
 	<!-- slider -->
 	
 	
-	<!-- 2nd row of products -->
+	<!-- 1st row of products -->
 
 	<section style="margin-top:50px;margin-bottom:50px;">
 		<div class="container-fluid" style="">
@@ -118,14 +301,207 @@
 						</div>
 					</div>
 				</div>
-				
-				
 			</div>
 		</div>
 	</section>
 
 	<!-- 3rd row of products -->
+	<section style="margin-top:50px;margin-bottom:50px;">
+		<div class="container">
+			<div class="page-header">
+				<h1 id="timeline" style="text-align:center;">Our Story</h1>
+			</div>
+			<ul class="timeline">
+				<li>
+				  <div class="timeline-badge info"><img src="images/office-hand-finger-pointing-straight-thumb-2-12546.png" style="height:30px;"></div>
+				  <div class="timeline-panel">
+					<div class="timeline-heading">
+					  <h4 class="timeline-title">Bootstrap released</h4>
+					  <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> August 2011</small></p>
+					</div>
+					<div class="timeline-body">
+					  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+					  Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+					  dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+					  Aliquam in felis sit amet augue.</p>
+					</div>
+				  </div>
+				</li>
+				<li class="timeline-inverted">
+				  <div class="timeline-badge warning"><img src="images/717132.png" style="height:30px;"></i></div>
+				  <div class="timeline-panel">
+					<div class="timeline-heading">
+					  <h4 class="timeline-title">Bootstrap 2</h4>
+					</div>
+					<div class="timeline-body">
+					  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+					  Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+					  dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+					  Aliquam in felis sit amet augue.</p>
+					</div>
+				  </div>
+				</li>
+				<li>
+				  <div class="timeline-badge danger"><img src="images/homeicon.png" style="height:30px;"></i></div>
+				  <div class="timeline-panel">
+					<div class="timeline-heading">
+					  <h4 class="timeline-title">Left Event</h4>
+					  <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3 years ago</small></p>
+					</div>
+					<div class="timeline-body">
+					  <p>Add more progress events and milestones to the left or right side of the timeline. Each event can be tagged with a date and given a beautiful icon to symbolize it's spectacular meaning.</p>
+					</div>
+				  </div>
+				</li>
+				<li class="timeline-inverted">
+				  <div class="timeline-badge default"><img src="images/homeicon.png" style="height:30px;"></div>
+				  <div class="timeline-panel">
+					<div class="timeline-heading">
+					  <h4 class="timeline-title">Right Event</h4>
+					</div>
+					<div class="timeline-body">
+					  <p>Add more progress events and milestones to the left or right side of the timeline. Each event can be tagged with a date and given a beautiful icon to symbolize it's spectacular meaning.</p>
+					</div>
+				  </div>
+				</li>
+			</ul>
+		</div>
+	</section>
 
+	<!-- 2nd row of products -->
+	<section style="margin-top:50px;margin-bottom:50px;">
+		<div class="container-fluid" style="">
+			<h1 class="center" style="text-align:center;">Our Partners</h1>
+			<p>&nbsp;</p>
+			<div class="row">
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+			</div>
+			<p>&nbsp;</p>
+			<div class="row">
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div style="text-align:center;">
+						<img src="images/alliance tariff plans unlimited.png" class="img-responsive" style="height:75px;">
+						<div class="product-details">
+							<!--<div class="caption">
+								<h5 style="text-align:center;font-size:18px;margin-top:15px;"><a href="#">FAST & FREE SHIPPING</a></h5>
+							</div>-->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 	
     
 	<footer style="margin-top:50px;">
---
+++ faq.php
@@ -0,0 +1,326 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <title>Eazyprint | About Us</title>
+
+    <link rel="shortcut icon" href="img/core-img/favicon.ico">
+
+    <script src="js/jquery-2.2.4.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/settings.css">
+    <link rel="stylesheet" type="text/css" href="css/layers.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+
+
+    <link rel="stylesheet" href="css/core-style.css">
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <link href="css/opensans.css" rel="stylesheet">
+
+
+    <script type="text/javascript" src="js/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="js/revolution.extension.navigation.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+	<style>
+		body{
+			background:#f5f5f5;
+		}
+		.pad0{
+			padding:0px;
+			margin:0px;
+		}
+		.shp_cart{
+			height:40px;background:rgb(42, 43, 46);width:40px;border-radius:50%;
+			margin-left:2px;text-align:center;
+		}
+		.shp_cart:hover{
+			cursor:pointer;
+			background:#dd0017;
+		}
+		.timeline {
+    list-style: none;
+    padding: 20px 0 20px;
+    position: relative;
+}
+
+    .timeline:before {
+        top: 0;
+        bottom: 0;
+        position: absolute;
+        content: " ";
+        width: 3px;
+        background-color: #eeeeee;
+        left: 50%;
+        margin-left: -1.5px;
+    }
+
+    .timeline > li {
+        margin-bottom: 20px;
+        position: relative;
+    }
+
+        .timeline > li:before,
+        .timeline > li:after {
+            content: " ";
+            display: table;
+        }
+
+        .timeline > li:after {
+            clear: both;
+        }
+
+        .timeline > li:before,
+        .timeline > li:after {
+            content: " ";
+            display: table;
+        }
+
+        .timeline > li:after {
+            clear: both;
+        }
+
+        .timeline > li > .timeline-panel {
+            width: 46%;
+            float: left;
+            border: 1px solid #d4d4d4;
+            border-radius: 2px;
+            padding: 20px;
+            position: relative;
+            -webkit-box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+            box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+        }
+
+            .timeline > li > .timeline-panel:before {
+                position: absolute;
+                top: 26px;
+                right: -15px;
+                display: inline-block;
+                border-top: 15px solid transparent;
+                border-left: 15px solid #ccc;
+                border-right: 0 solid #ccc;
+                border-bottom: 15px solid transparent;
+                content: " ";
+            }
+
+            .timeline > li > .timeline-panel:after {
+                position: absolute;
+                top: 27px;
+                right: -14px;
+                display: inline-block;
+                border-top: 14px solid transparent;
+                border-left: 14px solid #f5f5f5;
+                border-right: 0 solid #f5f5f5;
+                border-bottom: 14px solid transparent;
+                content: " ";
+            }
+
+        .timeline > li > .timeline-badge {
+            color: #fff;
+            width: 50px;
+            height: 50px;
+            line-height: 50px;
+            font-size: 1.4em;
+            text-align: center;
+            position: absolute;
+            top: 16px;
+            left: 50%;
+            margin-left: -25px;
+            background-color: #999999;
+            z-index: 100;
+            border-top-right-radius: 50%;
+            border-top-left-radius: 50%;
+            border-bottom-right-radius: 50%;
+            border-bottom-left-radius: 50%;
+        }
+
+        .timeline > li.timeline-inverted > .timeline-panel {
+            float: right;
+        }
+
+            .timeline > li.timeline-inverted > .timeline-panel:before {
+                border-left-width: 0;
+                border-right-width: 15px;
+                left: -15px;
+                right: auto;
+            }
+
+            .timeline > li.timeline-inverted > .timeline-panel:after {
+                border-left-width: 0;
+                border-right-width: 14px;
+                left: -14px;
+                right: auto;
+            }
+
+			.timeline-badge.primary {
+				background-color: #2e6da4 !important;
+			}
+
+			.timeline-badge.success {
+				background-color: #3f903f !important;
+			}
+
+			.timeline-badge.warning {
+				background-color: #f0ad4e !important;
+			}
+
+			.timeline-badge.danger {
+				background-color: #d9534f !important;
+			}
+
+			.timeline-badge.info {
+				background-color: #5bc0de !important;
+			}
+
+			.timeline-title {
+				margin-top: 0;
+				color: inherit;
+			}
+
+			.timeline-body > p,
+			.timeline-body > ul {
+				margin-bottom: 0;
+			}
+
+				.timeline-body > p + p {
+					margin-top: 5px;
+				}
+
+			@media (max-width: 767px) {
+				ul.timeline:before {
+					left: 40px;
+				}
+
+				ul.timeline > li > .timeline-panel {
+					width: calc(100% - 90px);
+					width: -moz-calc(100% - 90px);
+					width: -webkit-calc(100% - 90px);
+				}
+
+				ul.timeline > li > .timeline-badge {
+					left: 15px;
+					margin-left: 0;
+					top: 16px;
+				}
+
+				ul.timeline > li > .timeline-panel {
+					float: right;
+				}
+
+					ul.timeline > li > .timeline-panel:before {
+						border-left-width: 0;
+						border-right-width: 15px;
+						left: -15px;
+						right: auto;
+					}
+
+					ul.timeline > li > .timeline-panel:after {
+						border-left-width: 0;
+						border-right-width: 14px;
+						left: -14px;
+						right: auto;
+					}
+			}
+	</style>
+</head>
+
+<body>
+	<div class="container-fluid bg-dark">
+	as
+	</div>
+
+    <?php 
+		include("header.php");
+	?>
+	<!-- slider -->
+	
+	
+	<!-- 1st row of products -->
+
+	<section style="margin-top:50px;margin-bottom:50px;">
+		<div class="container-fluid" style="">
+			<h1 class="center" style="text-align:center;">FAQs</h1>
+			<p>&nbsp;</p>
+			<div class="row">
+				<div class="col-md-1"></div>
+				<div class="col-md-10">
+					<strong>You ship to my country ?</strong>
+					<p>We ship in India only.</p>
+
+					<strong>How much does shipping cost ?</strong>
+					<p>Shipping costs depend on the state of the buyer. Prices are displayed during checkout after a shipping address has been given.</p>
+
+					<strong>What is the displayed currency on your website ? can i pay with any other currency ? </strong>
+					<p>The prices are all displayed in INR (Indian Rupees).</p>
+
+					<strong>Is this product available ? </strong>
+					<p>Products displayed on our website are currently available. If a product becomes out of stock, we will update the website.</p>
+
+					<strong>If i order today, when will i receive my shipment ? </strong>
+					<p>We do our best to process every order within 1-2 business days. After processing your order, we will send you a shipping confirmation email for your convenience.</p>
+
+					<strong>My Order contains a mistake. what should i do? </strong>
+					<p>Please contact us as soon as you can at <strong style="color:red;">support@eazyprint.com</strong> and we will help you correct it. Please use this subject: "CHANGE ORDER: my order number is #99999" for the quickest possible response (including the order number in the subject line will help us locate your order quickly).</p>
+
+					<strong>I want to change something with my order, but it has already been processed ! what to do ? </strong>
+					<p>If you have just received the notification, there may still be time for us to change the order. Please contact us quickly at <strong style="color:red;">support@eazyprint.com</strong> and we will help you correct it. Please use this subject: "CHANGE ORDER: my order number is #99999" for the quickest possible response (including the order number in the subject line will help us locate your order quickly). If you received the notification a few hours ago, it is probably too late. If the address is incorrect, please email us the right address so that we may send the package again once it comes back to us.</p>
+
+					<strong>I did not receive my tracking number! what happened ? </strong>
+					<p>If you have not received your tracking number within 2 business days after receiving the shipment confirmation, please contact us at <strong style="color:red;">support@eazyprint.com</strong></p>
+
+					<strong>I want to cancel my order, but the order has already been shipped! What to do ? </strong>
+					<p>It is probably too late for us to stop the package. Please contact us at <strong style="color:red;">support@eazyprint.com</strong> to explain your situation, and we will see what we can do. You should also have a look at our return policy. <strong style="color:red;">Click here for returns page</strong></p>
+
+					<strong>The content of the shipment is not what i ordered ! </strong>
+					<p>We sometimes make mistakes and are sorry about that. Please contact us at <strong style="color:red;">support@eazyprint.com</strong> and give us your order number in the subject of the email. Please include pictures of the product and the back of the packaging. We will do our best to solve this issue for you quickly.</p>
+
+					<strong>What do i do if my order was damaged during shipping ? </strong>
+					<p>Sometimes the carrier mishandles the parcels and a product can be damaged during shipping. You should first try to open the package and see if the product inside is also damaged. If there is anything wrong with it, please contact us at <strong style="color:red;">support@eazyprint.com</strong> (you should include your order number in the subject of the email) and include pictures of the products and the packaging. We will do our best to accommodate you quickly.</p>
+
+					<strong>Where do i find my order number ? </strong>
+					<p>Your order number is the 5 digit number sent in the confirmation email. For example: #20340. If you cannot find your order number, you may tell us what email you provided when placing your order and we should be able to locate it. I cannot find an answer to my questions here. what should i do ? Contact us at <strong style="color:red;">support@eazyprint.com</strong> We will try to answer you as quickly as possible!</p>
+
+				</div>
+				<div class="col-md-1"></div>
+			</div>
+		</div>
+	</section>
+
+    
+	<footer style="margin-top:50px;">
+		<div class="container-fluid">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">s</div>
+					<div class="col-md-3">s</div>
+					<div class="col-md-3">s</div>
+					<div class="col-md-3">s</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+    <!-- ***** All jQuery Plugins ***** -->
+
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="js/include-all-plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+
+
+
+</body>
+</html>
