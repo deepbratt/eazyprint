ef702b2d655e827721499018697ad3890e648a65
Rajdeep Ghosh <ghoshrajdeep54@gmail.com>
2018-08-23 19:49:37 +0530

    index

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 
     <link rel="stylesheet" href="css/core-style.css">
     <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/boostrap/bootstrap.min.css">
 
 
     <link href="css/responsive.css" rel="stylesheet">
@@ -49,6 +50,129 @@
 			background:#dd0017;
 		}
 	</style>
+	<style>
+	h2{
+  text-align:center;
+  padding: 20px;
+}
+/* Slider */
+
+.slick-slide {
+    margin: 0px 20px;
+}
+
+.slick-slide img {
+    width: 100%;
+}
+
+.slick-slider
+{
+    position: relative;
+    display: block;
+    box-sizing: border-box;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+            user-select: none;
+    -webkit-touch-callout: none;
+    -khtml-user-select: none;
+    -ms-touch-action: pan-y;
+        touch-action: pan-y;
+    -webkit-tap-highlight-color: transparent;
+}
+
+.slick-list
+{
+    position: relative;
+    display: block;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+}
+.slick-list:focus
+{
+    outline: none;
+}
+.slick-list.dragging
+{
+    cursor: pointer;
+    cursor: hand;
+}
+
+.slick-slider .slick-track,
+.slick-slider .slick-list
+{
+    -webkit-transform: translate3d(0, 0, 0);
+       -moz-transform: translate3d(0, 0, 0);
+        -ms-transform: translate3d(0, 0, 0);
+         -o-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+}
+
+.slick-track
+{
+    position: relative;
+    top: 0;
+    left: 0;
+    display: block;
+}
+.slick-track:before,
+.slick-track:after
+{
+    display: table;
+    content: '';
+}
+.slick-track:after
+{
+    clear: both;
+}
+.slick-loading .slick-track
+{
+    visibility: hidden;
+}
+
+.slick-slide
+{
+    display: none;
+    float: left;
+    height: 100%;
+    min-height: 1px;
+}
+[dir='rtl'] .slick-slide
+{
+    float: right;
+}
+.slick-slide img
+{
+    display: block;
+}
+.slick-slide.slick-loading img
+{
+    display: none;
+}
+.slick-slide.dragging img
+{
+    pointer-events: none;
+}
+.slick-initialized .slick-slide
+{
+    display: block;
+}
+.slick-loading .slick-slide
+{
+    visibility: hidden;
+}
+.slick-vertical .slick-slide
+{
+    display: block;
+    height: auto;
+    border: 1px solid transparent;
+}
+.slick-arrow.slick-hidden {
+    display: none;
+}
+	</style>
+
 </head>
 
 <body>
@@ -145,7 +269,7 @@
 							</div>
 							
 							<div style="text-align:center;">
-								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Buy Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
+								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Order Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
 								<div class="clearfix"></div>
 							</div>
 						</div>
@@ -164,7 +288,7 @@
 							</div>
 							
 							<div style="text-align:center;">
-								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Buy Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
+								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Order Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
 								<div class="clearfix"></div>
 							</div>
 						</div>
@@ -183,7 +307,7 @@
 							</div>
 							
 							<div style="text-align:center;">
-								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Buy Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
+								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Order Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
 								<div class="clearfix"></div>
 							</div>
 						</div>
@@ -202,7 +326,7 @@
 							</div>
 							
 							<div style="text-align:center;">
-								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Buy Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
+								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Order Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
 								<div class="clearfix"></div>
 							</div>
 						</div>
@@ -226,7 +350,7 @@
 							</div>
 							
 							<div style="text-align:center;">
-								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Buy Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
+								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Order Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
 								<div class="clearfix"></div>
 							</div>
 						</div>
@@ -245,7 +369,7 @@
 							</div>
 							
 							<div style="text-align:center;">
-								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Buy Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
+								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Order Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
 								<div class="clearfix"></div>
 							</div>
 						</div>
@@ -264,7 +388,7 @@
 							</div>
 							
 							<div style="text-align:center;">
-								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Buy Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
+								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Order Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
 								<div class="clearfix"></div>
 							</div>
 						</div>
@@ -283,7 +407,7 @@
 							</div>
 							
 							<div style="text-align:center;">
-								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Buy Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
+								<a href="https://www.instamojo.com/@deepbratt/lb551355e76a9417b94758f945dc3dcb2/" rel="im-checkout" data-behaviour="remote" data-style="flat" data-text="Order Now"></a><script src="https://js.instamojo.com/v1/button.js"></script>
 								<div class="clearfix"></div>
 							</div>
 						</div>
@@ -298,12 +422,12 @@
 	
 	<!-- 2nd row of products -->
 
-<div class="container">
+<div class="container-fluid">
   <div id="myCarousel">
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="images/mobile_covers-01.jpg" alt="249">
+        <img src="images/mobile_covers-01.jpg" style="width:100%;">
       </div>
     </div>
   </div>
@@ -395,11 +519,11 @@
 			</div>
 		</div>
 	</section>
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
+<div class="jumbotron jumbotron-fluid" style="background:url(images/Studio-Rita-iPhone-cases-banner.jpg);height:300px;">
+  <!-- <div class="container">
     <h1 class="display-4">Personalized Mobile Cover</h1>
     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-  </div>
+  </div> -->
 </div>
 	<!-- 3rd row of products -->
 
@@ -489,6 +613,20 @@
 			</div>
 		</div>
 	</section>
+
+<div class="container-fluid" style="padding:50px;">
+  <h2 class="center" style="text-align:center;">Our <span class="badge badge-secondary" style="background-color:blue;">Partners</span></h2>
+   <section class="customer-logos slider">
+      <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
+   </section>
+</div>
     
 	<!-- <footer style="margin-top:50px;">
 		<div class="container-fluid">
@@ -506,8 +644,32 @@
 	include("footer.php");
 	?>
     <!-- ***** All jQuery Plugins ***** -->
-
+<script>
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+</script>
     <!-- Popper js -->
+	<script src="js/jquery-2.2.0.min.js"></script>
     <script src="js/popper.min.js"></script>
     <!-- Bootstrap js -->
     <script src="js/bootstrap.min.js"></script>
@@ -515,6 +677,7 @@
     <script src="js/include-all-plugins.js"></script>
     <!-- Active js -->
     <script src="js/active.js"></script>
+	<script src="js/slick.js"></script>
 
 
 
