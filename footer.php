3abc4aa4f7a28a5b6516d6284bd10fd541a1512b
Sujit Sarkar <sweezit92@gmail.com>
2018-08-24 14:28:16 +0530

    push footer

--- footer.php
+++ footer.php
@@ -1,30 +1,30 @@
-<footer class="bg-gray text-light footer-long" style="background-color: #343A40;border-top: 2px dashed #C82333;">
+<footer class="bg-gray text-light footer-long" style="background-color: #343A40;">
                 <div class="container">
                     <div class="row">
-                        <div class="col-12 col-md-3" style="margin-top:20px;">
+                        <div class="col-12 col-md-3" style="padding-top:50px;">
                             <img alt="Image" src="images/logo_phone.png" style="width:150px;" class="mb-4" />
 							<p><i class="fas fa-at"></i> support@sociolust.com</p>
 							<p><i class="fa fa-mobile-alt"></i> +91 987654321</p>
 							<p><a href="#" class="fab fa-facebook-square" style="font-size: 25px;text-decoration: none;color: white;"></a>&nbsp;&nbsp;&nbsp;<a href="#" class="fab fa-instagram" style="font-size: 25px;text-decoration: none;color: white;"></a>&nbsp;&nbsp;&nbsp;<a href="#" class="fab fa-twitter-square" style="font-size: 25px;text-decoration: none;color: white;"></a>&nbsp;&nbsp;&nbsp;<a href="#" class="fab fa-linkedin" style="font-size: 25px;text-decoration: none;color: white;"></a></p>
                         </div>
                         <!--end of col-->
-                        <div class="col-12 col-md-9" style="margin-top:20px;">
+                        <div class="col-12 col-md-9" style="padding-top:50px;">
                             <!-- <span class="h5">A robust suite of styled components for Bootstrap 4</span> -->
                             <div class="row no-gutters">
                                 <div class="col-6 col-lg-3">
                                     <h6>Products</h6>
                                     <ul class="list-unstyled">
                                         <li>
-                                            <a href="javascript:void(0);" style="color:#B9B9B9;">Overview</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Overview</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Landing Pages</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Landing Pages</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">App Pages</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">App Pages</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Inner Pages</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Inner Pages</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -33,16 +33,16 @@
                                     <h6>Platform</h6>
                                     <ul class="list-unstyled">
                                         <li>
-                                            <a href="javascript:void(0);">Mac OS &amp; iOS</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Mac OS &amp; iOS</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Android &amp; Chrome OS</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Android &amp; Chrome OS</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Windows</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Windows</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Linux</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Linux</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -51,19 +51,19 @@
                                     <h6>Community</h6>
                                     <ul class="list-unstyled">
                                         <li>
-                                            <a href="javascript:void(0);">Forum</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Forum</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Knowledgebase</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Knowledgebase</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Hire an expert</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Hire an expert</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">FAQ</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">FAQ</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Contact</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Contact</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -72,16 +72,16 @@
                                     <h6>Important Links</h6>
                                     <ul class="list-unstyled">
                                         <li>
-                                            <a href="javascript:void(0);">About company</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">About company</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">How it works</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">How it works</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Faq</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Faq</a>
                                         </li>
                                         <li>
-                                            <a href="javascript:void(0);">Contact us</a>
+                                            <a href="javascript:void(0);" style="text-decoration: none;color:#B9B9B9;">Contact us</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -91,11 +91,11 @@
                         </div>
                         <!--end of col-->
                     </div>
-					<div class="row">
+					<div class="row" style="margin-top: -20px;padding-bottom:20px;">
 						<div class="col-md-3">
 							&nbsp;
 						</div>
-						<div class="col-md-6 col-md-offset-3">
+						<div class="col-md-6">
 								&copy; 2018 Clickrstop Software Solutions Pvt Ltd | All rights reserve
 						</div>
 						<div class="col-md-3">
