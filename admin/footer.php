18cce424b5ee2a145395ac2d90697388a8fe7c70
Debashis Nath <debashisnath1992@gmail.com>
2018-08-25 14:22:01 +0530

    pushh

---
+++ admin/footer.php
@@ -0,0 +1,10 @@
+ <!--footer start-->
+      <footer class="site-footer">
+          <div class="text-center">
+              2018 &copy; Eazyprint by Clickrstop Software Solutions Pvt. Ltd.
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
+      <!--footer end-->
---
+++ admin/listing_brand.php
@@ -0,0 +1,95 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mosaddek">
+    <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <title>Eazyprint | View Brands</title>
+
+	<?php include("metalinks.php");?>
+    <!--dynamic table-->
+    <link href="css/demo_page.css" rel="stylesheet" />
+    <link href="css/demo_table.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/DT_bootstrap.css" />
+    
+  </head>
+
+  <body>
+
+  <section id="container" class="">
+      <?php include("header.php");?>
+      <?php include("sidebar.php");?>
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">
+              <!-- page start-->
+              <div class="row">
+                <div class="col-sm-12">
+              <section class="panel">
+              <header class="panel-heading">
+                  Brand Listing
+              </header>
+              <div class="panel-body">
+              <div class="adv-table">
+              <table class="display table table-bordered" id="hidden-table-info">
+              <thead>
+              <tr>
+                  <th>Sl No.</th>
+                  <th>Brand Name</th>
+				  <th>Models</th>
+                  <th class="hidden-phone">Action</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                  <td>1</td>
+                  <td>Nokia</td>
+				  <td><a href="listing_model.php" class="btn btn-info">View Models</a></td>
+                  <td class="hidden-phone"><div class="form-group" style="padding-right:15px;"><input type="button" class="btn btn-success" value="Edit" style="padding:6px 19px;"></div><div class="form-group"><input type="button" class="btn btn-danger" value="Delete"></div></td>
+              </tr>
+             
+              </tbody>
+              </table>
+
+              </div>
+              </div>
+              </section>
+              </div>
+              </div>
+              <!-- page end-->
+          </section>
+      </section>
+      <!--main content end-->
+     
+	<?php include("footer.php");?>
+  </section>
+
+    <!-- js placed at the end of the document so the pages load faster -->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-ui-1.9.2.custom.min.js"></script>
+    <script src="js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script type="text/javascript" language="javascript" src="js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="js/DT_bootstrap.js"></script>
+    <script src="js/respond.min.js" ></script>
+    <!--right slidebar-->
+    <script src="js/slidebars.min.js"></script>
+
+    <!--dynamic table initialization -->
+    <script src="js/dynamic_table_init.js"></script>
+
+
+    <!--common script for all pages-->
+    <script src="js/common-scripts.js"></script>
+
+  </body>
+</html>
