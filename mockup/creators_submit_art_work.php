7d8f60aa7d3bfd3e27871803f3bc2dcd48c11f6f
Debashis Nath <debashisnath1992@gmail.com>
2018-10-24 16:52:54 +0530

    pushh

---
+++ mockup/creators_submit_art_work.php
@@ -0,0 +1,69 @@
+
+<!doctype html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<meta name="msapplication-TileColor" content="#0061da">
+		<meta name="theme-color" content="#1643a3">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<link rel="icon" href="images/favicon.png" type="image/png"/>
+		<link rel="shortcut icon" type="image/png" href="images/favicon.png" />
+
+		<!-- Title -->
+		<title>Eazyprint | Submit Your Artwork</title>
+		<?php include("metalinks.php");?>
+		<link href="css/timeline.min.css" rel="stylesheet" />
+	</head>
+	<body class="app">
+
+				<?php include("header.php");?>
+
+				<div class="container">
+					<div class="side-app">
+						<p>&nbsp;</p>
+
+						<div class="col-md-12 col-sm-12 col-xs-12" style="padding:0px;margin:0px;">
+							<div class="card">
+								<div class="card-status bg-success br-tr-3 br-tl-3"></div>
+								<div class="card-body">
+									<h1 class="text-center">Submit Your Artwork</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="side-app">
+						<p>&nbsp;</p>
+						<div class="row">
+						<div class="col-md-6 col-sm-12 col-xs-12">
+								<div class="card">
+									<div class="card-status bg-success br-tr-3 br-tl-3"></div>
+									<div class="card-body" style="height:455px;">
+										<h1 class="text-center">How it works</h1>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-12 col-xs-12">
+								<div class="card">
+								<div class="card-status bg-success br-tr-3 br-tl-3"></div>
+									
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<?php include("footer.php");?>
+				
+	</body>
+</html>
