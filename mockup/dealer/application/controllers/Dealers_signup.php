f5930b3dd1b07a744357f33c6ae267a9c439e27b
Sujit Sarkar <sweezit92@gmail.com>
2018-10-31 13:26:33 +0530

    push

--- mockup/dealer/application/controllers/Dealers_signup.php
+++ mockup/dealer/application/controllers/Dealers_signup.php
@@ -8,6 +8,25 @@ class Dealers_signup extends CI_Controller {
 		$this->load->view('dealers_signup');
 	}
 
+	public function add_account()
+	{
+		$this->load->model('dealers_signup_m');
+		$first_name = $this->input->post("first_name");
+		$last_name = $this->input->post("last_name");
+		$email = $this->input->post("email");
+		$phone = $this->input->post("phone");
+		$state = $this->input->post("state");
+		$city = $this->input->post("city");
+		$address = $this->input->post("address");
+		$shop_name = $this->input->post("shop_name");
+		$gst = $this->input->post("gst");
+		$trade_license = $this->input->post("trade_license");
+		$date = time();
+
+		$check_exist = $this->dealers_signup_m->check_dealer($email,$phone);
+		
+	}
+
 }
 
 /* End of file Dealers_signup.php */
