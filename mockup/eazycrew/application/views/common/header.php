05ef39a47aff1f9e7bd67b4dc48ce0043477ba7f
Rajdeep Ghosh <ghoshrajdeep54@gmail.com>
2018-10-31 13:43:53 +0530

    admin

---
+++ mockup/eazycrew/application/views/common/header.php
@@ -0,0 +1,49 @@
+ <div id="global-loader" ><div class="showbox"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div></div>
+		<div class="page">
+			<div class="page-main">
+				<!-- Navbar-->
+				<header class="app-header header">
+
+					<!-- Header Background Animation-->
+					<div id="particles-js"  class=""></div>
+
+					<!-- Navbar Right Menu-->
+					<div class="container-fluid">
+						<div class="d-flex">
+							<a class="header-brand" href="index.html">
+								<img alt="ren logo" class="header-brand-img" src="<?php echo base_url();?>images/logo_w.png">
+							</a>
+							<!-- Sidebar toggle button-->
+							<a aria-label="Hide Sidebar" class="app-sidebar__toggle" data-toggle="sidebar" href="#"></a>
+							<div class="d-flex order-lg-2 ml-auto">
+								<div class="">
+									<form class="input-icon  mr-2">
+										<input class="form-control header-search" placeholder="Search&hellip;" tabindex="1" type="search">
+										<div class="input-icon-addon">
+											<i class="fe fe-search"></i>
+										</div>
+									</form>
+								</div>
+								<div class="dropdown">
+									<a class="nav-link pr-0 leading-none d-flex" data-toggle="dropdown" href="#">
+										<span class="avatar avatar-md brround" style="background-image: url(assets/images/faces/female/25.jpg)"></span>
+										<span class="ml-2 d-none d-lg-block">
+											<span class="text-white">Sindy Scribner</span>
+										</span>
+									</a>
+									<div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+										<a class="dropdown-item" href="#"><i class="dropdown-icon mdi mdi-account-outline"></i> Profile</a>
+										<a class="dropdown-item" href="#"><i class="dropdown-icon mdi mdi-settings"></i> Settings</a>
+										<a class="dropdown-item" href="#"><span class="float-right"><span class="badge badge-primary">6</span></span> <i class="dropdown-icon mdi mdi-message-outline"></i> Inbox</a>
+										<a class="dropdown-item" href="#"><i class="dropdown-icon mdi mdi-comment-check-outline"></i> Message</a>
+										<div class="dropdown-divider"></div>
+										<a class="dropdown-item" href="#"><i class="dropdown-icon mdi mdi-compass-outline"></i> Need help?</a>
+										<a class="dropdown-item" href="login.html"><i class="dropdown-icon mdi mdi-logout-variant"></i> Sign out</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</header>
+				
+				
