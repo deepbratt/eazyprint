ed632c390855f268f7aa42c3de32a721c59f78f8
Sujit Sarkar <sweezit92@gmail.com>
2018-11-12 20:52:15 +0530

    push

--- mockup/eazycrew/application/controllers/Admin_add_product_material_type.php
+++ mockup/eazycrew/application/controllers/Admin_add_product_material_type.php
@@ -2,30 +2,47 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin_add_product_material_type extends CI_Controller {
+	
+	function __construct(){
+        parent::__construct();
+        if(!$this->session->userdata['logged_in']['user_id']){
+            redirect('admin_login');
+        }
+    }
 
 	public function index()
 	{
 		$this->load->model('admin_add_product_material_type_m');
 		$this->load->view('admin_add_product_material_type');
 	}
+
 	public function add_product_material_type()
 	{
 		$this->load->model('admin_add_product_material_type_m');
 		$sub_cat_name = $this->input->post('sub_cat');
 		$product_material_type_name = $this->input->post('product_material_type');
 		$sub_cat_id = "1";
-		$records=array('product_material_type'=>$product_material_type_name,'sub_category_id'=>$sub_cat_name);
-		$insert_product_material_type = $this->admin_add_product_material_type_m->product_material_type_insert($records);
-		if($insert_product_material_type)
+
+		$check_material = $this->admin_add_product_material_type_m->check_material($sub_cat_name,$product_material_type_name);
+		if($check_material < 1)
 		{
-			$this->session->set_flashdata("success", "You have successfully insert the product metarial type!");
-			redirect('admin_add_product_material_type');	
-		}
-		else
-		{
-			$this->session->set_flashdata("failed", "Something went wrong!");
+			$records=array('product_material_type'=>$product_material_type_name,'sub_category_id'=>$sub_cat_name);
+			$insert_product_material_type = $this->admin_add_product_material_type_m->product_material_type_insert($records);
+			if($insert_product_material_type)
+			{
+				$this->session->set_flashdata("success", "You have successfully insert the product metarial type!");
+				redirect('admin_add_product_material_type');	
+			}
+			else
+			{
+				$this->session->set_flashdata("failed", "Something went wrong!");
+				redirect('admin_add_product_material_type');
+			}
+		}else{
+			$this->session->set_flashdata("exist", "This product material type already exist!");
 			redirect('admin_add_product_material_type');
 		}
+		
 	}
 
 }
