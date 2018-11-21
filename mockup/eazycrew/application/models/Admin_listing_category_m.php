893095b48b61637b1469629d78ce12a43ad50b51
Sujit Sarkar <sweezit92@gmail.com>
2018-11-21 15:32:40 +0530

    push

--- mockup/eazycrew/application/models/Admin_listing_category_m.php
+++ mockup/eazycrew/application/models/Admin_listing_category_m.php
@@ -3,11 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin_listing_category_m extends CI_Model {
 
-	public function fetch_category($category_status)
+	public function fetch_category()
 	{
 		$this->db->select('*');
 		$this->db->from('category');
-		$this->db->where('category_status', $category_status);
 		$query = $this->db->get();
 		return $query->result();
 		
@@ -17,6 +16,13 @@ class Admin_listing_category_m extends CI_Model {
 	{
 		$this->db->where('category_id', $category_id);
 		$this->db->delete('category');
+		return true;
+	}
+
+	public function update_category($cat_id,$update_array)
+	{
+		$this->db->where('category_id', $cat_id);
+		$this->db->update('category', $update_array);
 		return true;
 	}
 }
