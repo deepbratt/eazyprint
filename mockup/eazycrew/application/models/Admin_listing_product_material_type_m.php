9d3d50c4bafda12ccce528b38bbb58f3463541fb
Sujit Sarkar <sweezit92@gmail.com>
2018-11-22 14:22:45 +0530

    push

--- mockup/eazycrew/application/models/Admin_listing_product_material_type_m.php
+++ mockup/eazycrew/application/models/Admin_listing_product_material_type_m.php
@@ -11,14 +11,25 @@ class Admin_listing_product_material_type_m extends CI_Model {
 		return $query->result();
 		
 	}
-	public function cat_name($cat_sub_id)
+
+	public function sub_category($sub_id)
 	{
 		$this->db->select('*');
 		$this->db->from('sub_category');
-		$this->db->where('sub_category_id', $cat_sub_id);
+		$this->db->where('sub_category_id', $sub_id);
 		$query = $this->db->get();
 		return $query->row();
 	}
+
+	public function get_cat($cat_id)
+	{
+		$this->db->select('*');
+		$this->db->from('category');
+		$this->db->where('category_id', $cat_id);
+		$query = $this->db->get();
+		return $query->row();
+	}
+
 	public function product_material_type_dlt($product_material_type_id)
 	{
 		$this->db->where('product_material_id', $product_material_type_id);
--- mockup/eazycrew/application/views/admin_listing_product_material_type.php
+++ mockup/eazycrew/application/views/admin_listing_product_material_type.php
@@ -17,6 +17,29 @@
 
 		<!-- Title -->
 		<title>Eazyprint | Listing Product Metarial Type</title>
+		<style>
+		b{
+			color:black;
+			font-weight:1000;
+			font-size:20px;
+			font-family:arial;
+		}
+		th{
+			color:black;
+			font-weight:bold !important;
+			font-size:20px;
+			font-family:arial;
+			background:#ECEEF9;
+		}
+		.card-body{
+			zoom: 65%;
+		}
+		td{
+			color:black;
+			font-family:arial;
+			font-size:18px;
+		}
+		</style>
 
 <?php
 $this->load->view("common/metalinks");
@@ -58,20 +81,18 @@ $this->load->view("common/sidebar");
 						<div class="row">
 							<div class="col-md-12 col-lg-12">
 								<div class="card">
-									<div class="card-status bg-yellow br-tr-3 br-tl-3"></div>
-									<div class="card-header">
-										<div class="card-title">Listing Product Metarial Type</div>
-									</div>
 									<div class="card-body">
 										<div class="table-responsive">
 											<table id="example" class="table table-striped table-bordered" style="width:100%">
 												<thead>
 												  <tr>
-													<th class="wd-15p">Product Metarial Type Id
+													<th class="wd-15p">Sl&nbsp;No
 													</th>
-													<th class="wd-15p">Subcategory Name
+													<th class="wd-15p">Category&nbsp;Name
 													</th>
-													<th class="wd-20p">Product Metarial Type Name
+													<th class="wd-15p">Subcategory&nbsp;Name
+													</th>
+													<th class="wd-20p">Metarial&nbsp;Name
 													</th>
 													<th class="wd-15p">Action
 													</th>
@@ -87,18 +108,24 @@ $this->load->view("common/sidebar");
 													<td><?php echo $i; ?>
 													</td>
 													<td>
+													<?php
+														$sub_id = $product_material_type_fetch->sub_category_id;
+														$sub_category = $this->admin_listing_product_material_type_m->sub_category($sub_id);
+														$get_cat = $this->admin_listing_product_material_type_m->get_cat($sub_category->parent_cat_id);
+														echo $get_cat->category_name;
+													?>
+													</td>
+													<td>
 													<?php 
-														$cat_sub_id = $product_material_type_fetch->sub_category_id;
-														$this->load->model('admin_listing_product_material_type_m');
-														$sub_category = $this->admin_listing_product_material_type_m->cat_name($cat_sub_id);
 														echo $sub_category->sub_category_name;
 													?>
 													</td>
 													<td><?php echo $product_material_type_fetch->product_material_type;?>
 													</td>
 													<td>
-													<a href="<?php echo base_url('admin_edit_product_material_type');?>/<?php echo $product_material_type_fetch->product_material_id;?>" class="btn btn-primary">Edit</a>
-													<a href="<?php echo base_url('admin_listing_product_material_type');?>/dlt_product_material_type/<?php echo $product_material_type_fetch->product_material_id;?>" class="btn btn-primary">Delete</a>
+														<a href="<?php echo base_url('admin_edit_product_material_type/');?><?php echo $product_material_type_fetch->product_material_id;?>"><img src="<?php echo base_url('images/Edit.png');?>" style="height:30px"></a>&nbsp;&nbsp;
+
+														<a href="<?php echo base_url('admin_listing_product_material_type/dlt_product_material_type/');?><?php echo $product_material_type_fetch->product_material_id;?>"><img src="<?php echo base_url('images/Delete.png');?>" style="height:30px"></a>
 													</td>
 												  </tr>
 												<?php $i++; } ?>
