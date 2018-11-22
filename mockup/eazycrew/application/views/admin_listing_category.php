ef77ec8c04ffdef35d19efe80b42e3cf90247ea1
Debashis Nath <debashisnath1992@gmail.com>
2018-11-22 15:05:22 +0530

    Update admin_listing_category.php

--- mockup/eazycrew/application/views/admin_listing_category.php
+++ mockup/eazycrew/application/views/admin_listing_category.php
@@ -17,7 +17,29 @@
 
 		<!-- Title -->
 		<title>Eazyprint | Listing Category</title>
-
+		<style>
+			b{
+				color:black;
+				font-weight:1000;
+				font-size:20px;
+				font-family:arial;
+			}
+			th{
+				color:black;
+				font-weight:bold !important;
+				font-size:20px;
+				font-family:arial;
+				background:#ECEEF9;
+			}
+			.card-body{
+				zoom: 65%;
+			}
+			td{
+				color:black;
+				font-family:arial;
+				font-size:18px;
+			}
+		</style>
 <?php
 $this->load->view("common/metalinks");
 ?>
@@ -48,6 +70,7 @@ $this->load->view("common/sidebar");
 						<div class="alert alert-success success_div" style="display:none;">
 							<strong>Status Changed!</strong>
 						</div>
+						<div id="AjaxLoader" class="alert success_div" style="display:none;"><strong style="margin-left:-24px;"><img src="<?php echo base_url();?>images/ajax-loader2.gif" style="padding:5px;">Please Wait...</strong></div>
 						<?php
 						  if($this->session->flashdata('failed')){
 						?>
@@ -61,10 +84,8 @@ $this->load->view("common/sidebar");
 						<div class="row">
 							<div class="col-md-12 col-lg-12">
 								<div class="card">
-									<div class="card-status bg-yellow br-tr-3 br-tl-3"></div>
-									<div class="card-header">
-										<div class="card-title">Listing Category</div>
-									</div>
+									
+									
 									<div class="card-body">
 										<div class="table-responsive">
 											<table id="example" class="table table-striped table-bordered" style="width:100%">
@@ -148,9 +169,18 @@ $this->load->view("common/footer");
 		        url: '<?php echo base_url();?>admin_listing_category/change_status',
 		        data: {'cat_id': cat_id,'cat_status':cat_status}, // change this to send js object
 		        type: "post",
+		         beforeSend: function(){
+		        	$('.success_div').hide();
+			        $('#AjaxLoader').show();
+			    },
+			    complete: function(){
+			    	$('.success_div').show();
+			        $('#AjaxLoader').hide();
+			    },
 		        success: function(response){
 				 $('.switch_'+cat_id+'').html(response);
 				 $('.success_div').show();
+				 $('#AjaxLoader').hide();
 				 $('html, body').animate({scrollTop:$('.page-header').position().top}, 'slow');
 		        }
 		      });
