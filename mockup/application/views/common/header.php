e895abd17bcf367629163c283ee000813fe91b97
Sujit Sarkar <sweezit92@gmail.com>
2018-11-24 17:08:42 +0530

    push

--- mockup/application/views/common/header.php
+++ mockup/application/views/common/header.php
@@ -2,54 +2,45 @@
 		<div class="page">
 			<div class="page-main">
 				<!-- Navbar-->	
-					<header class="app-header header shadow-none relative" style="background:#f3f4f8;border-bottom:1px solid #d5def0;">
+					<header class="app-header header shadow-none fixed-header relative" style="background:#f3f4f8;border-bottom:1px solid #d5def0;">
 						<div class="container">
 						
 						<!-- Navbar Right Menu-->
 						<div class="container-fluid">
-							<div class="d-flex">
-								<a class="header-brand" href="<?php echo base_url();?>home.php">
-									<img alt="ren logo" class="header-brand-img" src="<?php echo base_url();?>images/logo_header.png" style="height:50px;">
-								</a>
-								<div class="d-flex order-lg-3 ml-auto" style="margin-top:5px;">
-									
-									<!-- before login -->
-
-										<a class="nav-link pr-0 leading-none d-flex" href="#">
-											<span class="ml-2 d-none d-lg-block">
-												<span class="text-dark"> <b> LOGIN </b> </span>
-											</span>
-										</a>
-
-
-										<a class="nav-link pr-0 leading-none d-flex" href="#">
-											<span class="ml-2 d-none d-lg-block">
-												<span class="text-dark"> <b> SIGNUP </b> </span>
-											</span>
-										</a>
-		
-
-									<!-- before login ends -->
-
-									<!-- after login -->
-									<div class="dropdown">
-										<a class="nav-link pr-0 leading-none d-flex" data-toggle="dropdown" href="#">
-											<span class="avatar avatar-md brround" style="background-image: url(images/25.jpg)"></span>
-											<span class="ml-2 d-none d-lg-block">
-												<span class="text-dark"> <b>Sindy S.</b></span>
-											</span>
-										</a>
-										<div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-											<a class="dropdown-item" href="#"><i class="dropdown-icon mdi mdi-account-outline"></i> Profile</a>
-											<a class="dropdown-item" href="#"><i class="dropdown-icon mdi mdi-settings"></i> Settings</a>
-											<a class="dropdown-item" href="#"><span class="float-right"><span class="badge badge-primary">6</span></span> <i class="dropdown-icon mdi mdi-message-outline"></i> Inbox</a>
-											<a class="dropdown-item" href="#"><i class="dropdown-icon mdi mdi-comment-check-outline"></i> Message</a>
-											<div class="dropdown-divider"></div>
-											<a class="dropdown-item" href="#"><i class="dropdown-icon mdi mdi-compass-outline"></i> Need help?</a>
-											<a class="dropdown-item" href="login.html"><i class="dropdown-icon mdi mdi-logout-variant"></i> Sign out</a>
-										</div>
-									</div>
-									<!-- after login ends -->
+							<div class="d-flex" style="height: 17px;">
+								<div class="d-flex order-lg-3 mr-auto">
+									<a class="nav-link d-flex" href="#">
+										<span class="d-none d-lg-block" style="font-size: 11px;">
+											<span class="text-dark"> <b> Delights </b> </span>
+										</span>
+									</a>
+									<a class="nav-link d-flex" href="#">
+										<span class="d-none d-lg-block" style="font-size: 11px;">
+											<span class="text-dark"> <b> Fanbook </b> </span>
+										</span>
+									</a>
+									<a class="nav-link d-flex" href="#">
+										<span class="d-none d-lg-block" style="font-size: 11px;">
+											<span class="text-dark"> <b> Download App </b> </span>
+										</span>
+									</a>
+								</div>
+								<div class="d-flex order-lg-3 ml-auto">
+									<a class="nav-link d-flex" href="#">
+										<span class="d-none d-lg-block" style="font-size: 11px;">
+											<span class="text-dark"> <b> Contact Us </b> </span>
+										</span>
+									</a>
+									<a class="nav-link  d-flex" href="#">
+										<span class="d-none d-lg-block" style="font-size: 11px;">
+											<span class="text-dark"> <b> Track Order </b> </span>
+										</span>
+									</a>
+									<a class="nav-link  d-flex" href="#">
+										<span class="d-none d-lg-block" style="font-size: 11px;">
+											<span class="text-dark"> <b> Pay online & get free shipping. </b> </span>
+										</span>
+									</a>
 								</div>
 								<a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse" data-target="#headerMenuCollapse" style="margin-top:12px;">
 									<span class="header-toggler-icon" style="color:#000;"></span>
@@ -63,9 +54,8 @@
 				<div class="ren-navbar fixed-header" id="headerMenuCollapse">
 					<div class="container">
 						<div class="row">
-							<a class="nav-link" href="#" style="text-align:center">
-								<i class="fas fa-home"></i>
-								<span> HOME </span>
+							<a class="nav-link header-brand" href="<?php echo base_url();?>home.php">
+								<img alt="ren logo" class="header-brand-img" src="<?php echo base_url();?>images/logo_header.png" style="height:50px;">
 							</a>
 						
 							<ul class="nav pull-left">
