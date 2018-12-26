f8f10491d37ad4919fbc46e6e2d3e44294595af9
Debashis Nath <debashisnath1992@gmail.com>
2018-12-26 18:52:43 +0530

    Update Admin_add_product_color.php

--- mockup/eazycrew/application/controllers/Admin_add_product_color.php
+++ mockup/eazycrew/application/controllers/Admin_add_product_color.php
@@ -39,8 +39,6 @@ class Admin_add_product_color extends CI_Controller {
 	{
 		$this->load->model('admin_add_product_color_m');
 		$sub_cat_id = $this->input->post('sub_category');
-		echo $sub_cat_id;
-		exit;
 		$product_color_name = $this->input->post('product_color');
 		$color_code_name = $this->input->post("color_code");
 
