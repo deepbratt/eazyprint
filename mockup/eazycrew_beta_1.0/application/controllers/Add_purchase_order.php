e17e2b8642c7e89cf1adaeaac35886808853b163
Debashis Nath <debashisnath1992@gmail.com>
2019-01-03 16:49:37 +0530

    pushh

--- mockup/eazycrew_beta_1.0/application/controllers/Add_purchase_order.php
+++ mockup/eazycrew_beta_1.0/application/controllers/Add_purchase_order.php
@@ -3,10 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Add_purchase_order extends CI_Controller {
 
+	function __construct(){
+        parent::__construct();
+        if(!$this->session->userdata['logged_in']['user_id']){
+            redirect('admin_login');
+        }
+    }
+
 	public function index()
 	{
 		$this->load->model('purchase_order_m');
-		$data['fetch_admin_info'] = $this->purchase_order_m->admin_info('');
+		$user_id = $this->session->userdata['logged_in']['user_id'];
+		$data['fetch_admin_info'] = $this->purchase_order_m->admin_info($user_id);
 		$this->load->view('add_purchase_order');
 	}
 
--- mockup/eazycrew_beta_1.0/application/models/Purchase_order_m.php
+++ mockup/eazycrew_beta_1.0/application/models/Purchase_order_m.php
@@ -3,7 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Purchase_order_m extends CI_Model {
 
-	
+	public function admin_info($user_id){
+
+		$this->db->select('*');
+		$this->db->from('eazycrew');
+		$this->db->where('crew_id',$user_id);
+		$this->db->where('crew_status','1');
+		$query = $this->db->get();
+		return $query->row();
+	}
 
 }
 
