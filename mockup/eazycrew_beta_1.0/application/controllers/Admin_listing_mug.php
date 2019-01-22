7bd1b72c141c1251e6178a4a0e43946dada98a6e
Sujit Sarkar <sweezit92@gmail.com>
2019-01-22 19:07:51 +0530

    push

---
+++ mockup/eazycrew_beta_1.0/application/controllers/Admin_listing_mug.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin_listing_mug extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('admin_listing_mug');
+	}
+
+}
+
+/* End of file Admin_listing_mug.php */
+/* Location: ./application/controllers/Admin_listing_mug.php */
