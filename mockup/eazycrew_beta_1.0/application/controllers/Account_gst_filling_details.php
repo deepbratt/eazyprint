465f9da1a88b3f7a91bdb5c44d7fbf789ec076b1
Debashis Nath <debashisnath1992@gmail.com>
2019-01-30 15:25:08 +0530

    gst filling details

---
+++ mockup/eazycrew_beta_1.0/application/controllers/Account_gst_filling_details.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account_gst_filling_details extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('account_gst_filling_details');
+	}
+
+}
+
+/* End of file Account_gst_filling_details.php */
+/* Location: ./application/controllers/Account_gst_filling_details.php */
