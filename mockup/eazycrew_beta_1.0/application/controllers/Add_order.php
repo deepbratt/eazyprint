3e5ce542eeef1c6d694b5fc6bdd246a0e08f6312
rex2cool <ghoshrajdeep54@gmail.com>
2019-02-21 13:02:50 +0530

    order

---
+++ mockup/eazycrew_beta_1.0/application/controllers/Add_order.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Add_order extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('orders/add_order');
+	}
+
+}
+
+/* End of file add_tshirt.php */
+/* Location: ./application/controllers/add_tshirt.php */
