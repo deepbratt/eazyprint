ccc532e388b9b5a6ed63001100b2e5f24f3f2c65
Debashis Nath <debashisnath1992@gmail.com>
2019-03-20 19:58:09 +0530

    Update login.php

--- mockup/application/views/login.php
+++ mockup/application/views/login.php
@@ -258,8 +258,8 @@ $this->load->view("common/header");
                         <div class="row">
                           <div class="col-md-12 col-sm-12 gst-label divMargin" id="divGSTN">
                             <div class="group">
-                              <input type="email" class="form-control landing_input_text upper-case sd-input error-input" placeholder="Email OR Username" tabindex="10" autocomplete="new-email" name="username" required="required">
-                              <input type="password" class="form-control landing_input_text upper-case sd-input error-input" placeholder="Password" tabindex="10" autocomplete="new-password" name="password" required="required" style="margin-top:3px;">
+                              <input type="email" class="form-control landing_input_text sd-input error-input" placeholder="Email OR Username" tabindex="10" autocomplete="new-email" name="username" required="required">
+                              <input type="password" class="form-control landing_input_text sd-input error-input" placeholder="Password" tabindex="10" autocomplete="new-password" name="password" required="required" style="margin-top:3px;">
                               <span class="bar">
                               </span>
                             </div>
