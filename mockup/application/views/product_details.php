7eee0dd50af3ca6a138d1a934ee74f71523b13c3
Deep Bratt Mondal <deepbratt@gmail.com>
2019-03-29 19:39:44 +0530

    das
    dasads

--- mockup/application/views/product_details.php
+++ mockup/application/views/product_details.php
@@ -601,8 +601,108 @@ $this->load->view("common/header");
 							</div>
 						</div>
 						<h1>&nbsp;</h1>
+						<?php 
+							if(isset($fetch_raw_data->raw_category) && $fetch_raw_data->raw_category == 2){
+						?>
+						<div class="col-md-12" style="margin:0px;padding:0px;">
+							<div class="col-md-6" style="float:left;margin:0px;padding:0px;">
+								<h3 style="text-align:center;margin-bottom:10px;color:#000;">MUGS PRICING FOR BULK</h3>
+								<p>&nbsp;</p>
+								<table class="table table-bordered table-condensed">
+									<thead>
+										<tr>
+											<th style="color:#000;">Qty</th>
+											<th style="color:#000;">White Mug<br>Price/Piece</th>
+											<th style="color:#000;">Color Mug<br>Price/Piece</th>
+											<th style="color:#000;">Magic Mug<br>Price/Piece</th>
+										</tr>
+									</thead>
+									<tbody>
+											<tr>
+												<td style="color:#000;">Upto 4</td>
+												<td>Rs. 249</td>
+												<td>Rs. 329</td>
+												<td>Rs. 449</td>
+											</tr>
+											<tr>
+												<td style="color:#000;">5 to 9</td>
+												<td>Rs. 229</td>
+												<td>Rs. 299</td>
+												<td>Rs. 429</td>
+											</tr>
+											<tr>
+												<td style="color:#000;">10 to 29</td>
+												<td>Rs. 199</td>
+												<td>Rs. 279</td>
+												<td>Rs. 399</td>
+											</tr>
+											<tr>
+												<td style="color:#000;">30 to 49</td>
+												<td>Rs. 189</td>
+												<td>Rs. 249</td>
+												<td>Rs. 379</td>
+											</tr>
+											<tr>
+												<td style="color:#000;">50+</td>
+												<td>Rs. 179</td>
+												<td>Rs. 229</td>
+												<td>Rs. 349</td>
+											</tr>
+									</tbody>
+								</table>
+								<p>get </p>
+							</div>
+							<div class="col-md-6" style="float:left;margin:0px;padding:0px;text-align:center;">
+								<h3 style="text-align:center;margin-bottom:10px;color:#000;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MUGS SPECIFICATION</h3>
+								<p>&nbsp;</p>
+								<img src="<?php echo base_url();?>/images/mug-specs.png" style="text-align:center;">
+							</div>
+							
+							
+							<div class="clearfix"></div>
+						</div>
 
+						<hr />
 
+						<div class="col-md-12" style="margin:0px;padding:0px;">
+							<div class="col-md-4" style="float:left;margin:0px;padding:0px;">
+								<div class="col-md-2" style="float:left;">
+									<img src="<?php echo base_url();?>images/mugs-icon-01.png" alt="Mugs Printing Online Fast Delivery" title="Mugs Printing Online Fast Delivery" style="margin-top:10px;">
+								</div>
+								<div class="col-md-10" style="float:left;padding:10px;">
+									<h4>100% Satisfaction Guaranteed</h4>
+									<p>We continuously strive hard to achieve your satisfaction as Our Top Priority.</p>
+								</div>
+							</div>
+							<div class="col-md-4" style="float:left;margin:0px;padding:0px;">
+								<div class="col-md-2" style="float:left;">
+									<img src="<?php echo base_url();?>images/mugs-icon-02.png" alt="Mugs Printing Online Fast Delivery" title="Mugs Printing Online Fast Delivery" style="margin-top:10px;">
+								</div>
+
+								<div class="col-md-10" style="float:left;padding:10px;">
+									<h4>Superior Print Quality</h4>
+									<p>High quality resolution imaging, quality mugs with multi-color printing..</p>
+								</div>
+							</div>
+							<div class="col-md-4" style="float:left;margin:0px;padding:0px;">
+								<div class="col-md-2" style="float:left;">
+									<img src="<?php echo base_url();?>images/mugs-icon-03.png" alt="Mugs Printing Online Fast Delivery" title="Mugs Printing Online Fast Delivery" style="margin-top:10px;">
+								</div>
+
+								<div class="col-md-10" style="float:left;padding:10px;">
+									<h4>Fast Turnaround Times</h4>
+									<p>Order printing, packing, and handing over to courier in 2-3 business days.</p>
+								</div>
+							</div>
+					
+
+							<div class="clearfix"></div>
+						</div>
+
+						<hr />
+						<?php
+							}
+						?>
 						<p>&nbsp;</p>
 						<h1 class="page-title" style="color:black;text-align:left;font-size:45px;font-family:samarkan1;"> Similar Products </h1>
 						<p>&nbsp;</p>
