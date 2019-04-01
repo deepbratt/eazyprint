4e4bfc545af8121c269ed8b60f7988ce8103a7e2
himadri majumder <himadrimajumder8@gmail.com>
2019-04-01 15:31:05 +0530

    hyoo

---
+++ mockup/admin/application/models/Add_order_summery_m.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Add_order_summery_m extends CI_Model {
+
+	public function fetch_pro($raw_id)
+	{
+		$this->db->select('*');
+		$this->db->from('raw_materials');
+		$this->db->where('raw_id',$raw_id);
+		$query = $this->db->get();
+		return $query->row();
+	}
+
+	public function delete_pro($product_id)
+	{
+		$query = $this->db->query("delete from products where product_id='".$product_id."'");
+		return true;
+	}
+
+	public function update_status($product_id,$update_array)
+	{
+		$this->db->where('product_id', $product_id);
+		$this->db->update('products', $update_array);
+		return true;
+	}
+
+	public function get_images($pro_id)
+	{
+		$this->db->select('*');
+		$this->db->from('product_image');
+		$this->db->where('product_id',$pro_id);
+		$query = $this->db->get();
+		return $query->row();
+	}
+
+	public function get_name($pro_cat_id)
+	{
+		$this->db->select('*');
+		$this->db->from('category');
+		$this->db->where('cat_id', $pro_cat_id);
+		$query = $this->db->get();
+		return $query->row();
+	}
+}
