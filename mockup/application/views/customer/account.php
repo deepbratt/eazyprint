200d8a48a651e9e2ecf1a878bd431c8686b371e7
Sujit Sarkar <sweezit92@gmail.com>
2019-04-01 19:47:11 +0530

    update

--- mockup/application/views/customer/account.php
+++ mockup/application/views/customer/account.php
@@ -30,12 +30,26 @@ $this->load->view("common/metalinks");
         color:#fff !important;
         margin-top:8px !important;
       }
+	  .get_back:hover{
+	  color: #F66175;
+	  }
+	 .field-icon {
+	  float: right;
+	  margin-right: 10px;
+	  margin-top: -29px;
+	  position: relative;
+	  font-size:15px;
+	  z-index: 2;
+	 }
     </style>
   </head>
   <body class="app">
     <?php
 $this->load->view("common/header");
 ?>
+	<!-- Status starts here -->
+	<input type="hidden" id="status" value="<?php echo $this->session->flashdata('status');?>">
+	<!-- Status ends here -->
     <div class="container">
       <div class="side-app">
         <p>&nbsp;
@@ -50,43 +64,43 @@ $this->load->view("common/header");
                     <!-- Tabs -->
                     <ul class="nav panel-tabs">
                       <li >
-                        <a href="#personal" class="active" data-toggle="tab">Personal info
+                        <a href="#personal" class="active" id="p_info" data-toggle="tab">Personal info
                         </a>
                       </li>
                       <li class="">
-                        <a href="#address" data-toggle="tab">Billing Address
+                        <a href="#address" id="b_info" data-toggle="tab">Billing Address
                         </a>
                       </li>
                       <li>
-                        <a href="#password" data-toggle="tab">Password Change
+                        <a href="#password" id="pass_info" data-toggle="tab">Password Change
                         </a>
                       </li>
                     </ul>
                   </div>
                 </div>
                 <?php
-        				  if($this->session->flashdata('success')){
-        				?>
-        					<div class="alert alert-success">
-        						<strong><?php echo $this->session->flashdata('success');?></strong>
-        					</div>
-        				<?php
-        				}
-        				if($this->session->flashdata('failed')){
-        				?>
-        					<div class="alert alert-danger">
-        						<strong><?php echo $this->session->flashdata('failed');?></strong>
-        					</div>
-        				<?php
-        				}
-        				if($this->session->flashdata('exist')){
-        				?>
-        					<div class="alert alert-warning">
-        						<strong><?php echo $this->session->flashdata('exist');?></strong>
-        					</div>
-        				<?php
-        				}
-        				?>
+				  if($this->session->flashdata('success')){
+				?>
+					<div class="alert alert-success">
+						<strong><?php echo $this->session->flashdata('success');?></strong>
+					</div>
+				<?php
+				}
+				if($this->session->flashdata('failed')){
+				?>
+					<div class="alert alert-danger">
+						<strong><?php echo $this->session->flashdata('failed');?></strong>
+					</div>
+				<?php
+				}
+				if($this->session->flashdata('exist')){
+				?>
+					<div class="alert alert-warning">
+						<strong><?php echo $this->session->flashdata('exist');?></strong>
+					</div>
+				<?php
+				}
+				?>
                 <div class="panel-body tabs-menu-body">
                   <div class="tab-content">
                     <div class="tab-pane active " id="personal">
@@ -96,7 +110,6 @@ $this->load->view("common/header");
                             <div class="row">
                                <div class="col-md-4">
                                 <div class="form-group">
-                                  <label class="form-label">Profile Image</label>
                                      <?php
                                          if($fetch_account_details->user_profile_image == ''){
                                      ?>
@@ -105,12 +118,12 @@ $this->load->view("common/header");
                                         <strong style="color:#000000;padding:10px;font-size:15px;">Choose File...</strong>
                                       </span>
                                       <input type="file" name="profile_image" id="pro_image" class="form-control" style="display:none;" onchange="show_image(this);">
-                                      <img src="" onclick="profile_imagezz()" style="height:150px;display:none;" id="p_blah">
+                                      <img src="" onclick="profile_imagezz()" style="height:150px;display:none;border-radius: 50%;" id="p_blah">
                                       <?php
                                         }else{
                                       ?>
                                         <input type="file" name="profile_image" id="pro_image" class="form-control" style="display:none;" onchange="show_image(this);">
-                                        <img src="<?php echo base_url('uploads/user_profile_image/');?><?php echo $fetch_account_details->user_profile_image;?>" onclick="profile_imagezz()" style="height:150px;" id="p_blah">
+                                        <img src="<?php echo base_url('uploads/user_profile_image/');?><?php echo $fetch_account_details->user_profile_image;?>" onclick="profile_imagezz()" style="height:150px;border-radius: 50%;" id="p_blah">
                                       <?php
                                       }
                                       ?>
@@ -136,7 +149,7 @@ $this->load->view("common/header");
                                 <div class="form-group">
                                   <label class="form-label">Email address
                                   </label>
-                                  <input type="email" class="form-control" name="email"  placeholder="Email" value="<?php echo $fetch_account_details->user_email;?>" required>
+                                  <input type="email" class="form-control" name="email"  placeholder="Email" value="<?php echo $fetch_account_details->user_email;?>" readonly>
                                 </div>
                               </div>
                               <div class="col-sm-6 col-md-12">
@@ -168,7 +181,7 @@ $this->load->view("common/header");
                                 <div class="form-group">
                                   <label class="form-label">Address
                                   </label>
-                                  <input type="text" class="form-control" name="address" placeholder="Address" value="<?php echo $fetch_account_details->user_address;?>">
+                                  <textarea class="form-control" name="address" placeholder="Address" ><?php echo $fetch_account_details->user_address;?></textarea>
                                 </div>
                               </div>
                               <div class="col-sm-6 col-md-6">
@@ -214,21 +227,37 @@ $this->load->view("common/header");
                       <div class="card">
                         <div class="card-body">
                           <form  method="post" method="POST" action="<?php echo base_url('account/update_password');?>">
-                            
-                              <p class="text-muted">Enter your email address and your password will be reset and emailed to you.
-                              </p>
+                             
+							  
+							 
+                              <label class="form-label">Enter old password
+                              </label>
                               <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" name="email_id" value="<?php echo $fetch_account_details->user_email;?>">
+                                <input type="password" class="form-control" id="password-field" placeholder="Enter old password" name="old_pass" required>
+								<span toggle="#password-field" class="fas fa-eye field-icon toggle-password"></span>
                               </div>
-                              <div class="form-footer">
-                                <button type="submit" class="btn btn-primary btn-block">Send me new password
+							  <label class="form-label">Enter new password
+                              </label>
+                              <div class="form-group">
+                                <input type="password" class="form-control" id="password_1" placeholder="Enter new password" name="new_pass" required>
+								<span toggle="#password_1" class="fas fa-eye field-icon toggle-password"></span>
+                              </div>
+							  <label class="form-label">Re-enter new password
+                              </label>
+                              <div class="form-group">
+                                <input type="password" class="form-control" id="password_2" placeholder="Re-enter new password" name="con_pass" required>
+								<span toggle="#password_2" class="fas fa-eye field-icon toggle-password"></span> 
+                              </div>
+							  <div id="errors" class=""></div>
+                              <div class="form-footer text-right">
+                                <button type="submit" class="btn btn-primary">Send me new password
                                 </button>
                               </div>
                             
                           </form>
-                          <div class="text-center text-muted mt-3 ">
+                          <div class="text-center form-label mt-3 ">
                             Forget it, 
-                            <a href="<?php echo base_url('login');?>">send me back
+                            <a class="get_back" href="<?php echo base_url('login');?>">send me back
                             </a> to the sign in screen.
                           </div>
                         </div>
@@ -247,6 +276,32 @@ $this->load->view("common/footer");
 ?>
   </body>
 </html>
+<script type="text/javascript" src="<?php echo base_url();?>js/jquery.password-validation.js"></script>
+<script>
+	$(document).ready(function() {
+		$("#password_1").passwordValidation({"confirmField": "#password_2"}, function(element, valid, match, failedCases) {
+
+		    $("#errors").html("<pre>" + failedCases.join("\n") + "</pre>");
+		  
+		     if(valid) $(element).css("border","2px solid green");
+		     if(!valid) $(element).css("border","2px solid red");
+		     if(valid && match) $("#password_2").css("border","2px solid green");
+		     if(!valid || !match) $("#password_2").css("border","2px solid red");
+			 if(valid) $("#errors").hide();
+		});
+	});
+</script>
+<script>
+	$(".toggle-password").click(function() {
+	  $(this).toggleClass("fa-eye-slash");
+	  var input = $($(this).attr("toggle"));
+	  if (input.attr("type") == "password") {
+		input.attr("type", "text");
+	  } else {
+		input.attr("type", "password");
+	  }
+	});
+</script>
 <script>
   function profile_imagezz(){
       $("input[id='pro_image']").click();
@@ -264,4 +319,21 @@ $this->load->view("common/footer");
       $('#hide_span').hide();
     }
   }
+</script>
+<script>
+$(document).ready(function() {
+	var status = $('#status').val();
+	if(status == 'password'){
+		 $("#p_info").removeClass("active");
+		 $("#personal").removeClass("active");
+		 $("#pass_info").addClass("active");
+		 $("#password").addClass("active");
+	}else if(status == 'address'){
+		 $("#p_info").removeClass("active");
+		 $("#personal").removeClass("active");
+		 $("#b_info").addClass("active");
+		 $("#address").addClass("active");
+	}
+		
+});
 </script>
