9555f16bbde628bdf179ba30d631b610cc26f9da
Debashis Nath <debashisnath1992@gmail.com>
2019-04-20 19:56:19 +0530

    pushhhhhh

--- mockup/application/controllers/Tshirt_editor.php
+++ mockup/application/controllers/Tshirt_editor.php
@@ -7,7 +7,7 @@ class Tshirt_editor extends CI_Controller {
 	{
 		$this->load->model('tshirt_editor_m');
 		//$data['raw_mat_id'] = $this->uri->segment(2);
-		$data['raw_mat_id'] = '41';
+		$data['raw_mat_id'] = '237';
 		$data['fetch_raw_data'] = $this->tshirt_editor_m->fetch_model_name($data['raw_mat_id']);
 		$data['fetch_similar_pro_data'] = $this->tshirt_editor_m->fetch_similar($data['raw_mat_id']);
 		$this->load->view('Tshirt_editor',$data);
