c0dc90937dab2dd34d073ec441597a99b79b7f5d
Sujit Sarkar <sweezit92@gmail.com>
2019-08-14 16:45:31 +0530

    update

--- beta/application/views/common/footer.php
+++ beta/application/views/common/footer.php
@@ -1,12 +1,24 @@
 <!-- footer ends -->
 <footer class="footer" style="background: #333;border-top:2px solid #ff9343;">
-      <div class="container">
+      <div class="container" style="margin-bottom:20px;">
         <div class="footer__page-links row">
           <div class="col-md-3">
             <h4 style="color:#ff9343;">ABOUT EAZYPRINT
             </h4>
             <ul>
               <li>
+                <a href="javascript:void(0);">Home
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0);">How it works
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0);">Clients/Vendors
+                </a>
+              </li>
+			   <li>
                 <a href="javascript:void(0);">Home
                 </a>
               </li>
@@ -26,6 +38,18 @@
             <ul>
               <li>
                 <a href="javascript:void(0);" target="_blank" rel="noopener">Pricing
+                </a>
+              </li>
+			   <li>
+                <a href="javascript:void(0);">Home
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0);">How it works
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0);">Clients/Vendors
                 </a>
               </li>
             </ul>
@@ -61,10 +85,11 @@
 			  </ul>
           </div>
 		  <div class="col-md-3">
-		  	<img src="<?php echo base_url('images');?>/logo-white.png" title="EazyPrint" alt="EazyPrint">
+		  	<img src="<?php echo base_url('images');?>/logo-white-1.png" title="EazyPrint" alt="EazyPrint" style="max-width:50% !important;">
+			<!-- <p style="color: #a6a6a6;text-align: -webkit-match-parent;font-size: 14px;" onMouseOver="this.style.color='#ff9343'" onMouseOut="this.style.color='#a6a6a6'">Kiran Dutta known as The Bong Guy by online name, is an Indian You-Tuber , Comedian and Internet Personality who posts comedy and Bollywood related videos to his channel. </p> -->
             <h4 style="color:#ff9343;">100% SECURE PAYMENT
             </h4>
-			<img class="paymentImg" src="<?php echo base_url('images/');?>secure-pay-1560341534.png" title="secure payments" alt="secure payments">
+			<img class="paymentImg" src="<?php echo base_url('images/');?>secure-pay-1560341534.png" title="secure payments" alt="secure payments" style="width: 100%;">
           </div>
         </div>
         <!-- <div>
