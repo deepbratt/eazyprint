efb90e068eee9f2b37d6abb0d2423d367206dd5c
himadri majumder <himadrimajumder8@gmail.com>
2019-08-17 18:56:30 +0530

    yoo

---
+++ beta/application/controllers/Terms.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Terms extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('terms');
+	}
+
+}
+
+/* End of file How_it_works.php */
+/* Location: ./application/controllers/How_it_works.php */
